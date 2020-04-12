6bec1885ada271f34746a23f74d6bfa0660e0af1
Rohit Thakur <rohitthakur2590@outlook.com>
2020-04-12 21:35:17 +0530

    collections playbooks
    Signed-off-by: Rohit Thakur <rohitthakur2590@outlook.com>

---
+++ collections/vyos/l3_interfaces/facts.py
@@ -0,0 +1,13 @@
+---
+- hosts: vyos
+  collections:
+    - vyos.vyos
+  gather_facts: yes
+  tasks:
+    - name: facts
+      vyos_facts:
+        gather_subset:
+          - '!all'
+          - '!min'
+        gather_network_resources:
+          - ospfv2
